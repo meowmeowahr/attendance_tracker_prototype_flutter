0ba5a6b6b88103970bed076d2736d2e9764df4b3
meowmeowahr <meowmeowahr@gmail.com>
2025-08-31 18:22:56 -0500

    fix green border after keyboard input

--- android/app/src/main/kotlin/com/example/attendance_tracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/attendance_tracker/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.attendance_tracker
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Disable Android default focus highlight for the root view
+        window?.decorView?.rootView?.defaultFocusHighlightEnabled = false
+    }
+}
